72d1faa59e988cb7a0a46a04c4c0674d580ddd97
Robin Hovind <robin.hanssen@gmail.com>
2021-11-12 13:32:36 +0100

    Add commands

---
+++ MyAwesomeConsoleGame/Commands/Move.cs
@@ -0,0 +1,22 @@
+﻿namespace MyAwesomeConsoleGame
+{
+    public class Move : Command
+    {
+        public readonly Direction Direction;
+
+        public Move(Direction direction, float duration ) : base(duration)
+        {
+            Direction = direction;
+        }
+
+        public override void Update(Rover rover)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsDone()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
