12ba3bae96c28983f306cd16f96149fb60b7b2a0
Sten Marius <sten.marius.holmen@gmail.com>
2021-11-12 08:15:51 +0100

    Add standard build configs

---
+++ MyAwesomeConsoleGame/MyAwesomeGame.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleEngine;
+using ConsoleEngine.Infrastructure;
+using Microsoft.Xna.Framework;
+
+namespace MyAwesomeConsoleGame
+{
+    public class MyAwesomeGame : GameBase
+    {
+        private Vector2 _objectPosition;
+        
+        public MyAwesomeGame() : base(
+            width: 40,
+            height: 40,
+            fontWidth: 14,
+            fontHeight: 14)
+        {
+            
+        }
+
+        protected override void OnInitialize()
+        {
+            _objectPosition = Console.ScreenCenter;
+        }
+
+        protected override void OnUpdate()
+        {
+            // GameTime holder info om tid siden forrige frame 
+            var timeSinceLastFrame = GameTime.Delta.TotalSeconds;
+            
+            
+        }
+        
+        protected override void OnRender()
+        {
+            
+        }
+    }
+}
---
+++ MyAwesomeConsoleGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyAwesomeConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
